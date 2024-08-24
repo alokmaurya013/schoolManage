75365d0b3b75b0ff57eec67ad597fa245122bddf
alokmaurya013 <106429374+alokmaurya013@users.noreply.github.com>
2024-08-24 17:18:43 +0530

    env change

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql=require('mysql2/promise');
-
+const password=process.env.PASSWORD
 const mySqlPool=mysql.createPool({
    host:"localhost",
    user:"root",
-   password:"alok8576",
+   password:password,
    database:"school_db"
 })
 module.exports=mySqlPool;
