61482edc76b3dbe561575ddec34a4aa7738394fb
alokmaurya013 <106429374+alokmaurya013@users.noreply.github.com>
2024-08-24 18:32:30 +0530

    env change3

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mysql=require('mysql2/promise');
 const host=process.env.HOST;
-const user=process.env.USER
+const user=process.env.USER;
 const password=process.env.PASSWORD;
 const database=process.env.DATABASE;
-
+const port=process.env.DB_PORT;
 const mySqlPool=mysql.createPool({
    host:host,
    user:user,
    password:password,
    database:database,
+   port:port
 })
 module.exports=mySqlPool;
